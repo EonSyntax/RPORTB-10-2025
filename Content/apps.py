42b1a6e81394ea2fe70c65d05e8ec51777b9b496
EonSyntax <eonsyntax@gmail.com>
2025-10-22 14:24:44 +0100

    working on prod

--- Content/apps.py
+++ Content/apps.py
@@ -1,6 +1,20 @@
+import os
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class ContentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Content'
+
+    def ready(self):
+        if os.environ.get("CREATE_SUPERUSER", "false").lower() == "true":
+            from .utils import create_admin_user
+
+            def create_user_after_migrate(sender, **kwargs):
+                try:
+                    create_admin_user()
+                except Exception as e:
+                    print(f"⚠ Superuser creation failed: {e}")
+
+            post_migrate.connect(create_user_after_migrate, weak=False)
